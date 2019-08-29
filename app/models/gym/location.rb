a5d22eebaca03891752998bacbfb37c1d26e59ac
github username <gregm9999@gmail.com>
2019-08-28 21:05:19 -0700

    Done.

--- app/models/gym/location.rb
+++ app/models/gym/location.rb
@@ -13,26 +13,6 @@ class Location
         @@all
     end
 
-    # def self.least_clients
-    #     trainer_num_clients_hash = {}
-    #     Trainer.all.each do |trainer|
-    #         if trainer_num_clients_hash[trainer.name]
-    #             trainer_num_clients_hash[trainer.name] +=1
-    #         else
-    #             trainer_num_clients_hash[trainer.name] =1
-    #         end
-    #     end
-    #     trainer_locations_array=[]
-    #     Trainer.all.each do |trainer|
-    #         trainer_locations_array << trainer.location.name
-    #     end
-    #     trainer_num_clients_hash.key(trainer_num_clients_hash.values.max)
-    #     x = Trainer.all.select do |trainer|
-    #         trainer.name == trainer_num_clients_hash.key(trainer_num_clients_hash.values.max)
-    #     end
-    #     x.first.location.name
-    # end
-
     def self.least_clients
         LocationTrainer.all.select do |lt|
             binding.pry
